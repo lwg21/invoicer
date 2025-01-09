d0a096b639c0dcabfc700b9fdff2fa1e7415be72
Lucas Gruner <lucas.gruner@gmail.com>
2025-01-09 11:35:53 +0100

    set up authentication with new rails g authentication generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_08_095101) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_09_094625) do
   create_table "clients", force: :cascade do |t|
     t.string "designation"
     t.string "address_line1"
@@ -45,4 +45,23 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_08_095101) do
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_invoices_on_client_id"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
